4322e0af1c7db6e113de8e11c0b6b084e5606e56
Weiran Ye <weiran@talkan.com>
2021-05-28 15:37:24 -0700

    Add collection view basis

--- TokenBoardGO/GoalViewController.swift
+++ TokenBoardGO/GoalViewController.swift
@@ -1,15 +1,41 @@
 import UIKit
 
-class GoalViewController: UIViewController {
+class GoalViewController: UIViewController, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var emojiCollection: UICollectionView!
+    
+    private let allEmojis = AllEmojis()
+    private var emojis: [Emoji] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        searchBar.delegate = self
+        emojiCollection.delegate = self
+        emojiCollection.dataSource = self
+        
+        emojis = allEmojis.filter(keyword: "")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
 
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        emojis = allEmojis.filter(keyword: searchText)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "emojiCell", for: indexPath)
+        return cell
+    }
 }
